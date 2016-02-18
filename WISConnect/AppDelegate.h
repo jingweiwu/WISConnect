026295009d1674bcad26a408530429c825494af5
吴经纬 <jingwei.wu@me.com>
2016-02-18 20:32:54 +0800

    Initial Commit

---
+++ WISConnect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WISConnect
+//
+//  Created by Jingwei Wu on 2/18/16.
+//  Copyright © 2016 Jingwei Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
